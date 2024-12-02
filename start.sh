3912520d6f06f4d67d9c7e0a9a7ff5be7f35831e
James <duyquangnguyenhac@gmail.com>
2024-12-01 16:28:02 -0800

    trying to make ssh work

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 DeveloperFolderPath=$(realpath ~/Developer)
 
-docker run -dit --rm --name dev_container -v $DeveloperFolderPath:/Developer -p 3000:3000 -p 8000:8000 dev_docker
+docker run -dit --rm --name dev_container -v $DeveloperFolderPath:/Developer -p 3000:3000 -p 8000:8000 -p 52020:22 dev_docker
 
